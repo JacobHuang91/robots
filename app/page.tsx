458fe8a1b648a1845169ed8e4806fa8155bc67ea
Xinghao Huang <jahuang@twilio.com>
2025-05-27 09:58:31 -0700

    add page12 links

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,12 @@ export default function Home() {
       <div>
         <a href="/private" target="_blank">private</a>
       </div>
+      <div>
+        <a href="/page1" target="_blank">page1</a>
+      </div>
+      <div>
+        <a href="/page2" target="_blank">page2</a>
+      </div>
     </div>
   );
 }
